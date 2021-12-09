42a54b63da644f0de3557b6a3ce8c428503cbf11
Ariane Silva Medina - AME <ariane.medina@iteris.com.br>
2021-12-09 11:53:00 -0300

    Function sendEmail concluded

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,46 +1,161 @@
 import React from "react";
 import styled from "styled-components";
-import InputText from '../Input';
+import Button from "../Button";
+import Github from "../../assets/github.png";
+import Linkedin from "../../assets/linkedin-logo.png";
+import emailjs from "emailjs-com";
 
 const FormStyles = styled.div`
   .form {
-    background-color: aquamarine;
     height: 50vh;
     width: 90vh;
   }
 
-  /* .input {
+  .input {
     background-color: #ece9e9;
     border-color: #ece9e9;
-    width: 50%;
+    width: 100%;
     height: 80px;
     font-size: 2rem;
     color: black;
     margin-top: 10px;
     margin: 4px;
-  } */
+    padding-left: 4%;
+  }
+
+  .subject {
+    background-color: #ece9e9;
+    border-color: #ece9e9;
+    width: 100%;
+    height: 80px;
+    font-size: 2rem;
+    color: black;
+    margin-top: 10px;
+    margin: 4px;
+    padding-left: 4%;
+  }
+
+  .message {
+    background-color: #ece9e9;
+    border-color: #ece9e9;
+    width: 100%;
+    height: 15rem;
+    font-size: 2rem;
+    color: black;
+    margin-top: 10px;
+    margin: 4px;
+    padding-left: 4%;
+  }
 
   .sideBySide {
     display: flex;
     justify-content: space-between;
-    flex-direction: row;
+    width: 95vh;
   }
+
+  .buttonDiv {
+    display: flex;
+    justify-content: flex-end;
+    height: 4vh;
+  }
+
+  .images {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 12%;
+  }
+
+  .git {
+    background-color: #ece9e9;
+    width: 7rem;
+    height: 7.2rem;
+    display: flex;
+    justify-content: center;
+  }
+
+  .linkedin {
+    background-color: #ece9e9;
+    width: 7rem;
+    height: 7.5rem;
+    display: flex;
+    justify-content: center;
+    border-radius: 20px;
+  }
+
 `;
 
 // Tenta usar GRID dps
 
 export default function Form() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_8tcaenh",
+        e.target,
+        "user_lc7iXVOXd0paSyzciC47S"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  }
+
   return (
     <FormStyles>
       <div className="form">
-        <div className="sideBySide">
-          <InputText placeholder="Name"  />
-          <InputText placeholder="Email" />
-        </div>
-
-        <InputText placeholder="Subject" />
-        <InputText placeholder="Message" />
+        <form onSubmit={sendEmail}>
+          <div className="sideBySide">
+            <input
+              type={"text"}
+              className="input"
+              placeholder="Name"
+              name="name"
+            />
+            <input
+              type={"email"}
+              className="input"
+              placeholder="Email"
+              name="email"
+            />
+          </div>
+          <input
+            type={"text"}
+            className="subject"
+            placeholder="Subject"
+            name="subject"
+          />
+          <textarea className="message" placeholder="Message" name="message" />
+          <div className="buttonDiv">
+            <Button buttonTitle={"Send"} type="submit" />
+          </div>
+        </form>
       </div>
+
+      <div className="images">
+        <a href="https://github.com/medynahs">
+          <div className="git">
+            <img src={Github} alt="Github" width={120} />
+          </div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/ariane-medina/">
+          <div className="linkedin">
+            <img src={Linkedin} alt="Linkedin" width={120} />
+          </div>
+        </a>
+      
+      </div>
+
+      
     </FormStyles>
   );
 }
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -6,8 +6,15 @@ const ContactStyles = styled.div`
   .title {
     font-size: 5rem;
     color: white;
-    justify-content: center;
-    display: flex;
+    text-align: center;
+    margin-bottom: 0px;
+  }
+
+  .subtitle {
+    font-size: 2rem;
+    color: white;
+    text-align: center;
+    margin-top: 0px;
   }
 
   .formDiv {
@@ -21,6 +28,7 @@ export default function Contact() {
   return (
     <ContactStyles>
      <p className='title'>Contact Me</p>
+     <p className='subtitle'>I am always eageared to be a part of new projects</p>
      <div className='formDiv'>
        <Form/>
      </div>
